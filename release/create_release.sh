d862692c7458541f27e976ba854e5ea524ac1016
werlang <pswerlang@gmail.com>
2022-09-26 18:32:19 +0000

    Release now composed by tar.gz files and .zip files.

--- release/create_release.sh
+++ release/create_release.sh
@@ -6,6 +6,10 @@ cd "$path/release" && node create_version.js
 cd "$path/worker" && pkg -C Brotli .
 rm -f "$path/worker/version.json"
 
+cd "$path/release" && tar -czf parlot-worker-linux.tar.gz parlot-worker-linux
+cd "$path/release" && tar -czf parlot-worker-macos.tar.gz parlot-worker-macos
+cd "$path/release" && tar -czf parlot-worker-windows.tar.gz parlot-worker-win.exe
+
 cd "$path/release" && zip -m parlot-worker-linux.zip parlot-worker-linux
 cd "$path/release" && zip -m parlot-worker-macos.zip parlot-worker-macos
 cd "$path/release" && zip -m parlot-worker-windows.zip parlot-worker-win.exe
